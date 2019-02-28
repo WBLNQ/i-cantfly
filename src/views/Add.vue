299098c9cc9911b5bc0c409126ca534f2baea485
Mr.liao <948540113@qq.com>
2019-02-28 09:20:24 +0800

    vuetest

---
+++ src/views/Add.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Add</div>
+</template>
+<script>
+export default {
+    name: 'Add'
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <button type="button" @click="reg()">登陆</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+<style scoped>
+
+</style>
